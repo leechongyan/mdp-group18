4cc48543c7505d79de71438821e6863bd71ea7cb
Xiaochen Jia <cjasonjia@gmail.com>
2020-01-24 13:03:55 +0800

    Android initial commit

---
+++ Android/app/src/main/java/ntu/mdp/grp18/MainActivity.java
@@ -0,0 +1,43 @@
+package ntu.mdp.grp18;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
+
+import ntu.mdp.grp18.fragments.BluetoothFragment;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment selectedFragment;
+
+                switch (menuItem.getItemId()){
+                    case R.id.icon1:
+                        selectedFragment = new BluetoothFragment();
+                        break;
+                    default:
+                        //TODO:Complete the switch statement
+                        selectedFragment = new BluetoothFragment();
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                return true;
+            }
+        });
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new BluetoothFragment()).commit();
+
+    }
+
+}
